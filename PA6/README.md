f50a83d48d6e1d9dced245986d1f9cae4dad280d
KC Rodrigue <kcrodrigue@KCs-MacBook-Pro.local>
2018-10-08 11:01:20 -0700

    Added PA6 with a CMake file

---
+++ PA6/README.md
@@ -0,0 +1,58 @@
+# PA5: Assimp Model Loading
+
+# Team Members:
+
+Thomas McCrone
+
+Kurtis Rodrigue
+
+Jeffrey Williams
+
+# Dependencies, Building, and Running
+
+### Assimp:
+
+Enter the following commands into terminal to install.
+
+Mac: 
+```bash
+brew install assimp
+```
+
+Linux: 
+```bash
+sudo apt-get install assimp
+```
+
+## Controls
+Reverse spin direction: A
+
+Reverse orbit direction: S
+
+## To Change The Model
+To change the model obj file, go to line 5 of object.cpp and change the string literal (assigned to obj_file) to the desired file directory. Currently it is set to ../src/board.obj and to switch to dragon it should be ../src/dragon.obj
+
+```bash
+mkdir build
+cd build
+cmake ..
+make
+./Tutorial
+```
+
+### Makefile Instructions 
+The makefile works as expected and must be updated with new files added in.
+
+```bash
+mkdir build
+cd build
+cp ../makefile .
+make
+./Tutorial
+```
+
+## Ubuntu.cse.unr.edu
+OpenGL 3.3 will run on the [ubuntu.cse.unr.edu](https://ubuntu.cse.unr.edu/) website. To do so follow the build instructions, but when running the Tutorial executable use this line to execute.
+```bash
+/usr/NX/scripts/vgl/vglrun ./Tutorial
+```
---
+++ PA6/src/object.cpp
@@ -0,0 +1,117 @@
+#include "object.h"
+
+Object::Object()
+{
+    std::string obj_file = "../assets/";
+    std::string file_name;
+    std::cout << "Please enter the name of the file you would like to load (include extension): " << std::endl;
+    std::cin >> file_name;
+    obj_file = obj_file + file_name;
+    LoadOBJ(obj_file.c_str());
+  /* The index works at a 0th index
+  for(unsigned int i = 0; i < Indices.size(); i++)
+  {
+    Indices[i] = Indices[i] - 1;
+  }*/
+
+  angle = 0.0f;
+  orbit_angle = 0.0f;
+  orbit_radius = 10;
+
+  glGenBuffers(1, &VB);
+  glBindBuffer(GL_ARRAY_BUFFER, VB);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * Vertices.size(), &Vertices[0], GL_STATIC_DRAW);
+
+  glGenBuffers(1, &IB);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * Indices.size(), &Indices[0], GL_STATIC_DRAW);
+}
+
+Object::~Object()
+{
+  Vertices.clear();
+  Indices.clear();
+}
+
+void Object::Update(unsigned int dt)
+{\
+    if(orbit_direction)
+    {
+        orbit_angle += dt * M_PI/1000;
+    }
+    else
+    {
+        orbit_angle -= dt * M_PI/1000;
+    }
+    if(spin_direction)
+    {
+        angle +=  dt * M_PI/10;
+    }
+    else
+    {
+        angle -=  dt * M_PI/10;
+    }
+
+    model = glm::translate(glm::mat4(1.0f), glm::vec3(orbit_radius * cos(orbit_angle),0,orbit_radius * sin(orbit_angle)));
+    model = glm::rotate(model, glm::radians(angle), glm::vec3(0.0, 1.0, 0.0));
+
+    if(orbit_angle > 360)
+    {
+        orbit_angle -= 360;
+    }
+    if(angle > 360)
+    {
+        angle -= 360;
+    }
+
+}
+
+glm::mat4 Object::GetModel()
+{
+  return model;
+}
+
+void Object::Render()
+{
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VB);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex,color));
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IB);
+
+  glDrawElements(GL_TRIANGLES, Indices.size(), GL_UNSIGNED_INT, 0);
+
+  glDisableVertexAttribArray(0);
+  glDisableVertexAttribArray(1);
+}
+
+void Object::LoadOBJ(const char* obj)
+{
+	Assimp::Importer importer;
+	std::ifstream fin(obj);
+
+	m_aiscene = importer.ReadFile(obj, aiProcess_Triangulate);
+
+	for (int i = 0; i < m_aiscene->mNumMeshes; i++) {
+		for (int j = 0; j < m_aiscene->mMeshes[i]->mNumVertices; j++) {
+			Vertices.push_back(Vertex(glm::vec3(float(m_aiscene->mMeshes[i]->mVertices[j].x),
+			                                    float(m_aiscene->mMeshes[i]->mVertices[j].y),
+			                                    float(m_aiscene->mMeshes[i]->mVertices[j].z)),
+			                          glm::vec3(float(m_aiscene->mMeshes[i]->mVertices[j].x),
+			                                    float(m_aiscene->mMeshes[i]->mVertices[j].y),
+			                                    float(m_aiscene->mMeshes[i]->mVertices[j].z))));
+		}
+
+	}
+	for (int i = 0; i < m_aiscene->mNumMeshes; i++) {
+		for (int j = 0; j < m_aiscene->mMeshes[i]->mNumFaces; j++) {
+				Indices.push_back(m_aiscene->mMeshes[i]->mFaces[j].mIndices[0]);
+				Indices.push_back(m_aiscene->mMeshes[i]->mFaces[j].mIndices[1]);
+				Indices.push_back(m_aiscene->mMeshes[i]->mFaces[j].mIndices[2]);
+
+		}
+	}
+}
